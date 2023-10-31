8406a9aea45f4a36a8e99591386fa6d392604173
Jean-Baptiste WITTNER <jbwittner.ext@hipay.com>
2023-10-31 17:08:51 +0100

    a lot of shit

---
+++ generation/java_server/src/main/java/fr/bankwiz/openapi/model/GroupAuthorizationEnum.java
@@ -0,0 +1,58 @@
+package fr.bankwiz.openapi.model;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import jakarta.annotation.Generated;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/**
+ * Gets or Sets GroupAuthorizationEnum
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
+public enum GroupAuthorizationEnum {
+  
+  READ("READ"),
+  
+  WRITE("WRITE"),
+  
+  ADMIN("ADMIN");
+
+  private String value;
+
+  GroupAuthorizationEnum(String value) {
+    this.value = value;
+  }
+
+  @JsonValue
+  public String getValue() {
+    return value;
+  }
+
+  @Override
+  public String toString() {
+    return String.valueOf(value);
+  }
+
+  @JsonCreator
+  public static GroupAuthorizationEnum fromValue(String value) {
+    for (GroupAuthorizationEnum b : GroupAuthorizationEnum.values()) {
+      if (b.value.equals(value)) {
+        return b;
+      }
+    }
+    throw new IllegalArgumentException("Unexpected value '" + value + "'");
+  }
+}
+
